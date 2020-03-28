df8fd2210e52f8fad28ec7db41c0ac75737e9830
IronicUsername <deniztoma512@gmail.com>
2020-03-28 11:51:39 +0100

    refactor trashTV script

--- js/script.js
+++ js/script.js
@@ -1,65 +1,146 @@
-async function getArchiData(){
+document.addEventListener('DOMContentLoaded', function() {setBkg()}, false);
+window.setInterval(function(){setBkg()}, 10000);  // call all 10 seconds
+document.onkeydown = function(e){_checkKey(e.keyCode)};
+
+var inLightMode = false;
+
+
+/**
+ * Get API pic data.
+ * @function _getArchiData
+ * @returns Dict[str, str]
+ */
+async function _getArchiData(){
     const url='https://archi.trash-economy.io/getTV';
     let resp = await fetch(url);
     let data = await resp.json()
     return data;
 }
 
+
+/**
+ * Set the background property for the children of div.#bkg.
+ * @function setBkg
+ */
 async function setBkg(){
-    const delay = 5000;
     const bkgElemRoot = document.getElementById('bkg');
     let bkg = bkgElemRoot.firstElementChild;
     let fg = bkgElemRoot.children[1];
 
-    let kaka = await getArchiData();
+    let kaka = await _getArchiData();
     bkg.style.backgroundImage = "url("+kaka['screenbg']+")";
     fg.style.backgroundImage = "url("+kaka['screenfg']+")";
     setTimeout(function(){
         bkg.style.backgroundImage = "url("+kaka['buffer']+")";
         fg.style.backgroundImage = "url("+kaka['buffer']+")";
-    }, 5000);  // read from the buffer; save API calls
+    }, 5000);  // read from the buffer every 5 seconds/half the time; save API calls
 
 }
 
-document.addEventListener('DOMContentLoaded', function() {setBkg()}, false);
-window.setInterval(function(){setBkg()}, 10000);  // rebuild all 10 seconds
+/**
+ * Checks which key was pressed and calls according function.
+ * [SPACE] = toggle between relax mode light and fullscreen
+ * @function _checkKey
+ * @param keyCode Keyboard Code
+ */
+function _checkKey(keyCode){
+    if(keyCode == 32){
+        _toggleBox();
+        _toggleBkg();
+        if(_checkFullscreen && !inLightMode) _toggleFullscreen();
+    }
+}
 
 
-function relaxMode(el, fullScreen, rmBox=false){
-    let element = el.parentNode.parentNode;
+/**
+ * Chill out.
+ * @function relaxMode
+ * @param goFullscreen: bool = decides if it goes into fullscreen or not
+ */
+function relaxMode(goFullscreen){
+    this.inLightMode = true;
+    _toggleBkg();
+    _toggleBox();
+    if(goFullscreen) _toggleFullscreen(), this.inLightMode = false;
+}
+
+
+/**
+ * Toggle visibility of the box.
+ * @function _toggleBox
+ */
+function _toggleBox(){
+    let box = document.getElementById('box-root');
+
+    if(box.style.display == 'block')box.style.display = 'none';
+    else box.style.display = 'block';
+}
+
+
+/**
+ * Toggle width and height of the background pics/gifs.
+ * @function _toggleBkg
+ */
+function _toggleBkg(){
     let bkgRoot = document.getElementById('bkg');
-    let bgStyle = bkgRoot.children[0];
-    let fgStyle = bkgRoot.children[1];
+    let bkgWidth = bkgRoot.style.width;
+    let bkgHeight = bkgRoot.style.height;
 
-    if(!document.fullscreenElement){
-        if(fullScreen){
-            if(rmBox) element.removeChild(el.parentNode);
-            _launchFullScreen(document.documentElement);
-        }
+    if((bkgWidth != '100%') && (bkgHeight != '100%')){
         bkgRoot.style.width = '100%';
         bkgRoot.style.height = '100%';
-        bkgRoot.style.transition = '.2s all ease-in-out';
-        element.style.display = 'none';
-    }
-
-    document.body.onkeyup = function(e){
-        if((e.keyCode = 32) || (e.keyCode = 27)){
-            if(!rmBox){
-                bkgRoot.style.removeProperty('width');
-                bkgRoot.style.removeProperty('height');
-                element.style.display = 'initial';
-                if(document.fullscreenElement) document.webkitExitFullscreen();
-            }
-        }
+    }else{
+        bkgRoot.style.removeProperty('width');
+        bkgRoot.style.removeProperty('height');
     }
 }
 
-function _launchFullScreen(element) {
-    if(element.requestFullScreen) {
-      element.requestFullScreen();
-    } else if(element.mozRequestFullScreen) {
-      element.mozRequestFullScreen();
-    } else if(element.webkitRequestFullScreen) {
-      element.webkitRequestFullScreen();
+
+/**
+ * Check if page is fullscreen.
+ * @function _checkFullscreen
+ */
+function _checkFullscreen(){
+    let isFullscreen = document.webkitIsFullScreen || document.mozFullScreen || false;
+    return isFullscreen ? true : false;
+}
+
+
+/**
+ * Toggle fullscreen.
+ * @function _toggleFullscreen
+ */
+function _toggleFullscreen(){
+    let webRoot = document.body;
+
+    webRoot.requestFullScreen = webRoot.requestFullScreen || webRoot.webkitRequestFullScreen || webRoot.mozRequestFullScreen || document.msRequestFullscreen || function () { return false; };
+    document.cancelFullScreen = document.cancelFullScreen || document.webkitCancelFullScreen || document.mozCancelFullScreen || document.msExitFullscreen || function () { return false; };
+    _checkFullscreen() ? document.cancelFullScreen() : webRoot.requestFullScreen();
+}
+
+
+/**
+ * Check what browser is used.
+ * @function _checkBrowser
+ * @return str
+ */
+function _checkBrowser(){
+    let isOpera = (!!window.opr && !!opr.addons) || !!window.opera || navigator.userAgent.indexOf(' OPR/') >= 0;
+    let isFirefox = typeof InstallTrigger !== 'undefined';
+    let isSafari = /constructor/i.test(window.HTMLElement) || (function (p) { return p.toString() === "[object SafariRemoteNotification]"; })(!window['safari'] || (typeof safari !== 'undefined' && safari.pushNotification));
+    let isChrome = !!window.chrome && (!!window.chrome.webstore || !!window.chrome.runtime);
+    let isIE = /*@cc_on!@*/false || !!document.documentMode;
+    let isEdge = !isIE && !!window.StyleMedia;
+    let isEdgeChromium = isChrome && (navigator.userAgent.indexOf("Edg") != -1);
+
+    switch(true){
+        case isOpera: return 'opera';
+        case isFirefox: return 'firefox';
+        case isSafari: return 'safari';
+        case isChrome: return 'chrome';
+        case isIE: return 'ie';
+        case isEdge: return 'edge';
+        case isEdgeChromium: return 'edgeCHROME';
+        default: return 'unknown';
     }
-  }
+}
